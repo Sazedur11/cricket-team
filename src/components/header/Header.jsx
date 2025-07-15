4450d46ceb93215a2a089ac7548bb72faaf55140
Sazedur <sazedur11ab@gmail.com>
2025-07-15 23:06:05 +0600

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
         <div className="flex flex-col items-center justify-center bg-cover bg-center rounded-xl p-10 "
             style={{ backgroundImage: `url(${headerBG})` }}>
             <img className='' src={banner} alt="Banner image" />
-            <h1 className='text-3xl font-bold text-white my-3'>Assemble Your Ultimate Dream 11 Football Team</h1>
-            <p className='text-gray-400 text-xl'>Beyond Boundaries Beyond Limits</p>
+            <h1 className='text-3xl font-bold text-white my-3'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+            <p className='text-gray-400 text-xl'>Beyond Boundaries Beyond Limits 'lets play the game'</p>
             <button className='bg-lime-400 rounded-sm font-bold px-2 py-1 mt-4'>Claim Free Credit</button>
         </div>
     );
